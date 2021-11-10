9c80b634eca8d9868b914d70e969fef5275facf1
Connor Andrew Ngo <91626039+CNgo0@users.noreply.github.com>
2021-11-10 10:39:33 -0500

    Example

---
+++ OARS_CS/Examples/Program.cs
@@ -0,0 +1,32 @@
+﻿using OARS_CS;
+using System;
+using System.IO;
+
+namespace Examples
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string key = File.ReadAllText("cngo.pem");
+
+            byte[] rawFile = OARS.Download("cngo", key, "test.txt", "development");
+
+            File.WriteAllBytes("test.txt", rawFile);
+
+            if (File.Exists("test.txt"))
+            {
+                string[] fileContents = File.ReadAllLines("test.txt");
+                foreach (string line in fileContents)
+                {
+                    Console.WriteLine(line);
+                }
+                File.Delete("test.txt");
+            }
+            else
+            {
+                Console.WriteLine("File was not downloaded!");
+            }
+        }
+    }
+}
